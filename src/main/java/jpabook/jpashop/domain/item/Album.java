6f298ecf388972c24453291a1e09db4101130fec
unknown <k509626@gmail.com>
2026-05-19 06:12:54 +0900

    First commit -springbootjpa

---
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Album extends Item{
+}
