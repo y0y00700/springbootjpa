fc6a5e46ba9b850927e7e4ef14458b62ded05ee5
권민혁 (선임) (주)더비소프트 (k5096262@cnspartner.com <k5096262@cns.lgcns.com>
2026-05-29 15:10:47 +0900

    Order / OrderItem Entity develope

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,4 +24,12 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
+
+    //==비즈니스 로직==//
+    public void cancel(){
+        getItem().addStock(count);
+    }
+    public int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
 }
