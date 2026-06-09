bd485e001957cf6da9b2a3602833df35910c1d3c
권민혁 (선임) (주)더비소프트 (k5096262@cnspartner.com <k5096262@cns.lgcns.com>
2026-06-09 12:56:42 +0900

    itemList developement

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -27,7 +27,6 @@ public class MemberController {
 
     @PostMapping("/members/new")
     public String create(@Valid MemberForm form, BindingResult result){
-
         if(result.hasErrors()){
             return "members/createMemberForm";
         }
